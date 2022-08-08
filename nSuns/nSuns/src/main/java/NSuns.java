c2cfac99de819619acb5539a7b946e8fc90212ee
OHJINSEONG <ojseong0828@naver.com>
2022-08-08 16:01:10 +0900

    완료 버튼 메서드화 전 백업

--- nSuns/nSuns/src/main/java/NSuns.java
+++ nSuns/nSuns/src/main/java/NSuns.java
@@ -1,14 +1,27 @@
 import models.User;
+import utils.SaveUserBody;
+
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
 
 public class NSuns {
 
     private JFrame frame;
     private JPanel contentPanel;
     private User user;
+    private int age;
+    private int height;
+    private int weight;
+    private String gender;
+    private JTextField heightTextField;
+    private JTextField ageTextField;
+    private JTextField weightTextField;
+
 
     public static void main(String[] args) {
         NSuns application = new NSuns();
@@ -35,7 +48,7 @@ public class NSuns {
 
     private void initMenuPanel() {
         JPanel menuPanel = new JPanel();
-        frame.add(menuPanel,BorderLayout.PAGE_START);
+        frame.add(menuPanel, BorderLayout.PAGE_START);
 
         JButton setCalorieButton = new JButton("기초 대사량");
         setCalorieButton.addActionListener(e -> {
@@ -43,9 +56,7 @@ public class NSuns {
 
             initSetCaloriePanel();
 
-            menuPanel.setVisible(false);
-            menuPanel.setVisible(true);
-            frame.setVisible(true);
+            updateDisplayPanel(menuPanel);
         });
         menuPanel.add(setCalorieButton);
 
@@ -57,17 +68,17 @@ public class NSuns {
         menuPanel.add(setWeightButton);
     }
 
+    private void updateDisplayPanel(JPanel Panel) {
+        Panel.setVisible(false);
+        Panel.setVisible(true);
+        frame.setVisible(true);
+    }
+
     private void initSetCaloriePanel() {
         JPanel panel = new JPanel();
         contentPanel.add(panel);
 
-        String gender = user.userGender();
-
-        int age = 0;
-        int height = 0;
-        int weight = 0;
-
-        panel.setLayout(new GridLayout(5,2));
+        panel.setLayout(new GridLayout(5, 2));
 
         JButton genderButton = new JButton("남성");
         genderButton.addActionListener(e -> {
@@ -83,32 +94,106 @@ public class NSuns {
 
         panel.add(new JLabel("나이(세) : "));
 
-        JTextField ageTextLabel = new JTextField(10);
-        panel.add(ageTextLabel);
+        ageTextField = new JTextField(10);
+        panel.add(ageTextField);
 
         panel.add(new JLabel("키(cm) : "));
 
-        JTextField heightTextField = new JTextField(10);
+        heightTextField = new JTextField(10);
         panel.add(heightTextField);
 
         panel.add(new JLabel("몸무게(kg) : "));
 
-        JTextField weightTextField = new JTextField(10);
+        weightTextField = new JTextField(10);
         panel.add(weightTextField);
 
         JButton goBackPageButton = new JButton("돌아가기");
+        goBackPageButton.addActionListener(e -> {
+            contentPanel.removeAll();
+
+            initMenuPanel();
+
+            updateDisplayPanel(contentPanel);
+        });
+
         panel.add(goBackPageButton);
 
-        JButton nextPageButton = new JButton("완료");
-        nextPageButton.addActionListener(e -> {
+
+        panel.add(nextPageButton);
+        panel.add(nextPage());
+    }
+
+    private JButton nextPage() {
+        JButton button = new JButton("완료");
+        button.addActionListener(e -> {
+
+
+            age = Integer.parseInt(ageTextField.getText());
+            height = Integer.parseInt(heightTextField.getText());
+            weight = Integer.parseInt(weightTextField.getText());
+            gender = user.userGender();
+
+//            user = new User(gender, age, height, weight);
+
             contentPanel.removeAll();
-            User user =new User(gender,age,height,weight);
+
+            SaveUserBody();
 
             initLifeStylePanel();
+
+            updateDisplayPanel(contentPanel);
+
         });
-        panel.add(nextPageButton);
+        return button;
     }
 
     private void initLifeStylePanel() {
+        JPanel panel = new JPanel();
+        contentPanel.add(panel);
+
+        JTextField timeTextField = new JTextField(10);
+        panel.add(timeTextField);
+    }
+
+    private void SaveUserBody() {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    SaveUserBody saveUserBody = new SaveUserBody(user);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//    JButton nextPageButton = new JButton("완료");
+//        nextPageButton.addActionListener(e -> {
+//
+//
+//                age = Integer.parseInt(ageTextField.getText());
+//                height = Integer.parseInt(heightTextField.getText());
+//                weight = Integer.parseInt(weightTextField.getText());
+//                gender = user.userGender();
+//
+////            user = new User(gender, age, height, weight);
+//
+//                contentPanel.removeAll();
+//
+//                SaveUserBody();
+//
+//                initLifeStylePanel();
+//
+//                updateDisplayPanel(contentPanel);
